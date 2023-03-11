0c2cba3145d208e9a87d2c00d9513e32a87f6030
Jimmy Tan <jimmytan0424@gmail.com>
2023-03-11 12:55:00 -0800

    middleware

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const express = require('express');
 
 const app = express();
 
+// Middleware functions
+app.use((req, res, next) => {
+  console.log('In the middleware!');
+
+  next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware!');
+  // ...
+});
+
 const server = http.createServer(app);
 
 // Listen to port 3000 --- http://localhost:3000/
