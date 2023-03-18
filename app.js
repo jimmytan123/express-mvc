20cdda07ad80a3c7858ceae51c0d46b71e11b64e
Jimmy Tan <jimmytan0424@gmail.com>
2023-03-18 13:34:28 -0700

    adding 404 page

--- app.js
+++ app.js
@@ -17,4 +17,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+// Catch all route
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page Not Found</h1>');
+});
+
 app.listen(3000);
