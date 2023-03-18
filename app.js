3709f44b5291b7af2b86c5a5e5ea98bbb01319e6
Jimmy Tan <jimmytan0424@gmail.com>
2023-03-18 13:31:41 -0700

    use Express Router

--- app.js
+++ app.js
@@ -4,25 +4,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// Import routes
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 // Register a middleware, do the body parsing for us
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // IMPORTANT: Express from v4.16 has body-parser implemented. And you can use:
 // app.use(express.urlencoded({ extended: true }));
 
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.post('/product', (req, res) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from express!</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
