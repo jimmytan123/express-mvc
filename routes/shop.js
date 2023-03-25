4592b0cadf131e1af31787d7e9cac13103f255b4
Jimmy Tan <jimmytan0424@gmail.com>
2023-03-25 09:41:58 -0700

    Use a helper function for file path navigation

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   //__dirname is an environment variable that tells you the absolute path of the directory containing the currently executing file.
-  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
---
+++ util/path.js
@@ -0,0 +1,4 @@
+const path = require('path');
+
+// Give the absolute path of the starter file (app.js)
+module.exports = path.dirname(require.main.filename);
