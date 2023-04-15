ffbdc067f2945f44f4dcdf6a63cdd3afb1394739
Jimmy Tan <jimmytan0424@gmail.com>
2023-04-15 16:24:50 -0700

    convert into template Handlebars

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const pug = require('pug');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
 // Set templating engine
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs());
+
+app.set('view engine', 'hbs');
 app.set('views', 'views'); // set views folder as where the views are stored
 // Import routes
 const adminData = require('./routes/admin');
@@ -27,7 +30,7 @@ app.use(shopRoutes);
 
 // Catch all route
 app.use((req, res, next) => {
-  res.status(404).render('404');
+  res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -12,7 +12,12 @@ router.get('/', (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 
   const products = adminData.products;
-  res.render('shop', { prods: products, docTitle: 'Shop' });
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+  });
 });
 
 module.exports = router;
