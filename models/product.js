38bf96fea9b4a76349edd82c6314f4fb538c0ef6
Jimmy Tan <jimmytan0424@gmail.com>
2023-04-22 15:03:13 -0700

    storing and fetching data via files

--- models/product.js
+++ models/product.js
@@ -1,16 +1,55 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+/* 
+    Helper variable for the path to the data/products.json
+*/
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
+);
+
+/* 
+    Helper Function for reading file - products
+    cb - callback Function
+*/
+const getProductsFromFile = (cb) => {
+  const p = path.join(
+    path.dirname(require.main.filename),
+    'data',
+    'products.json'
+  );
+
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
+  // Save the product to the file
   save() {
-    products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  // Static method - It is directly called from the class itself, not the instances
-  static fetchAll() {
-    return products;
+  /* 
+    Static method - It is directly called from the class itself, not the instances
+    Getting products from the file
+   */
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
