dba5a9b4f8a6d9346eb171796a90c13722a3a41e
Jimmy Tan <jimmytan0424@gmail.com>
2023-09-09 16:16:26 -0700

    sync to create tables

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -30,5 +31,13 @@ app.use(shopRoutes);
 // Catch all route
 app.use(errorController.get404);
 
-app.listen(3000);
-console.log('Listening in port 3000...');
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+    console.log('Listening in port 3000...');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- models/product.js
+++ models/product.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = require('../util/database');
 
+// https://sequelize.org/docs/v6/core-concepts/model-basics/
 const Product = sequelize.define('Product', {
   id: {
     type: Sequelize.INTEGER,
