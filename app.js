934e92a420ee7a9f8a3d590a6a939c603b5397a9
Jimmy Tan <jimmytan0424@gmail.com>
2024-03-18 11:01:05 -0700

    Creating and managing a dummy user

--- app.js
+++ app.js
@@ -23,8 +23,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // IMPORTANT: Express from v4.16 has body-parser implemented. And you can use:
 // app.use(express.urlencoded({ extended: true }));
 
-// Serviing static files(public folder)
+// Serving static files(public folder)
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Register middleware for setting user to the request
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      // Store user field to the request
+      req.user = user;
+
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 // Starting as /admin
 app.use('/admin', adminRoutes);
@@ -40,7 +52,18 @@ User.hasMany(Product);
 sequelize
   .sync() // Look at the defined models then create tables for them automatically. If exist, won't create/overwrite them.
   .then((result) => {
+    return User.findByPk(1);
     //console.log(result);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: 'Jim', email: 'test@test.com' });
+    }
+
+    return user;
+  })
+  .then((user) => {
+    console.log(user);
     app.listen(3000);
     console.log('Listening in port 3000...');
   })
