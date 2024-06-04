9b88f14ad1552a4dbe116bd93eb14e59e2310bb6
Jimmy Tan <jimmytan0424@gmail.com>
2024-06-04 11:05:01 -0700

    Display orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -119,8 +119,8 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  // Reach out to order model and query by user id
+  Order.find({ 'user.userId': req.user._id })
     .then((orders) => {
       res.render('shop/orders', {
         path: '/orders',
