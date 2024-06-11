8adee9b57f1c5a4640096baa2cd92961fb0cc02c
Jimmy Tan <jimmytan0424@gmail.com>
2024-06-11 16:55:32 -0700

    Add validation for edit product

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,7 @@ const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
+const { body } = require('express-validator');
 
 // /admin/add-product => GET
 router.get('/add-product', isAuth, adminController.getAddProduct);
@@ -14,13 +15,33 @@ router.get('/add-product', isAuth, adminController.getAddProduct);
 router.get('/products', isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', isAuth, adminController.postAddProduct);
+router.post(
+  '/add-product',
+  isAuth,
+  [
+    body('title').trim().isString().isLength({ min: 3 }),
+    body('imageUrl').isURL(),
+    body('price').trim().isFloat(),
+    body('description').trim().isLength({ min: 5, max: 150 }),
+  ],
+  adminController.postAddProduct
+);
 
 // /admin/edit-product/:productId => GET
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
 // /admin/edit-product/:productId => POST
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post(
+  '/edit-product',
+  isAuth,
+  [
+    body('title').trim().isString().isLength({ min: 3 }),
+    body('imageUrl').isURL(),
+    body('price').trim().isFloat(),
+    body('description').trim().isLength({ min: 5, max: 150 }),
+  ],
+  adminController.postEditProduct
+);
 
 // // /admin/delete-product => POST
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
