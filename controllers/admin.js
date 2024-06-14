aafb4ae4619c6012f7185f97885b9fe4cd91d9da
Jimmy Tan <jimmytan0424@gmail.com>
2024-06-14 12:50:40 -0700

    Update to delete product via client making http request

--- controllers/admin.js
+++ controllers/admin.js
@@ -183,8 +183,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
 
   Product.findById(prodId)
     .then((product) => {
@@ -198,11 +198,9 @@ exports.postDeleteProduct = (req, res, next) => {
       return Product.deleteOne({ _id: prodId, userId: req.user._id });
     })
     .then(() => {
-      res.redirect('/admin/products');
+      res.status(200).json({ message: 'Success!' }); // send back json data
     })
     .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ message: 'Deleting product failed!' });
     });
 };
