9781bb96a37f53f350dc654265e78235bd92c288
Jimmy Tan <jimmytan0424@gmail.com>
2024-07-09 11:33:06 -0700

    Set up ssl server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Import statement
 const path = require('path');
 const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -25,6 +26,9 @@ const store = new MongoDBStore({
   collection: 'sessions',
 });
 const csrfProtection = csrf();
+
+const privateKey = fs.readFileSync('server.key');
+const certificate = fs.readFileSync('server.cert');
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -151,6 +155,13 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
+
+    // Start a node server in HTTPS mode (manually without hosting provider help)
+    // https
+    //   .createServer({ key: privateKey, cert: certificate }, app)
+    //   .listen(process.env.PORT || 3000);
+    // console.log(`Server is running on port ${process.env.PORT || 3000}...`);
+
     app.listen(process.env.PORT || 3000);
     console.log(`Server is running on port ${process.env.PORT || 3000}...`);
   })
